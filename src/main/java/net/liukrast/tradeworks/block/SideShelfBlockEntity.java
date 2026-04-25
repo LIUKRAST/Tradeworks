6a4c0e7f7dcf596e8e05710d71a4285476fe96b1
LiukRast <darthlucavader@gmail.com>
2026-04-25 20:45:09 +0200

    Fixed #4

--- src/main/java/net/liukrast/tradeworks/block/SideShelfBlockEntity.java
+++ src/main/java/net/liukrast/tradeworks/block/SideShelfBlockEntity.java
@@ -2,6 +2,7 @@ package net.liukrast.tradeworks.block;
 
 import com.simibubi.create.content.logistics.tableCloth.TableClothBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -11,16 +12,41 @@ import net.minecraft.world.phys.BlockHitResult;
 public class SideShelfBlockEntity extends TableClothBlockEntity {
     public SideShelfBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
-        if(state.getBlock() instanceof SideShelfBlock)
-            facing = state.getValue(SideShelfBlock.HORIZONTAL_FACING);
+    }
+
+    @Override
+    public void lazyTick() {
+        super.lazyTick();
+        checkFacing();
+    }
+
+    public void checkFacing() {
+        if(getBlockState().getBlock() instanceof SideShelfBlock) {
+            var dir = getBlockState().getValue(SideShelfBlock.HORIZONTAL_FACING);
+            if(dir != facing) {
+                facing = dir;
+                setChanged();
+                sendData();
+            }
+        } else if(getBlockState().getBlock() instanceof ShelfBlock) {
+            var axis = getBlockState().getValue(ShelfBlock.HORIZONTAL_AXIS);
+            for(Direction dir : Direction.values()) {
+                if(dir.getAxis() == axis) {
+                    if(dir != facing) {
+                        facing = dir;
+                        setChanged();
+                        sendData();
+                    }
+                    break;
+                }
+            }
+        }
     }
 
     @Override
     public ItemInteractionResult use(Player player, BlockHitResult ray) {
         var res = super.use(player, ray);
-        var state = getBlockState();
-        if(state.getBlock() instanceof SideShelfBlock)
-            facing = state.getValue(SideShelfBlock.HORIZONTAL_FACING);
+        checkFacing();
         return res;
     }
 }
--- src/main/java/net/liukrast/tradeworks/mixin/TableClothBlockMixin.java
+++ src/main/java/net/liukrast/tradeworks/mixin/TableClothBlockMixin.java
@@ -3,15 +3,21 @@ package net.liukrast.tradeworks.mixin;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.simibubi.create.content.logistics.redstoneRequester.AutoRequestData;
 import com.simibubi.create.content.logistics.tableCloth.TableClothBlock;
+import com.simibubi.create.content.logistics.tableCloth.TableClothBlockEntity;
 import net.liukrast.tradeworks.TableClothPlacement;
 import net.liukrast.tradeworks.block.ShelfBlock;
 import net.liukrast.tradeworks.block.SideShelfBlock;
+import net.liukrast.tradeworks.block.SideShelfBlockEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TableClothBlock.class)
 public class TableClothBlockMixin {
@@ -19,6 +25,18 @@ public class TableClothBlockMixin {
     private int useItemOn(int original) {
         if(this instanceof TableClothPlacement tcp) return tcp.getPlacementId();
         return original;
+    }
+
+    @Inject(
+            method = "lambda$setPlacedBy$0",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/core/Direction;getOpposite()Lnet/minecraft/core/Direction;", shift = At.Shift.AFTER
+            )
+    )
+    private static void lambda$setPlacedBy$0(AutoRequestData requestData, Player player, TableClothBlockEntity dcbe, CallbackInfo ci) {
+        if(dcbe instanceof SideShelfBlockEntity ssbe)
+            ssbe.checkFacing();
     }
 
     @SuppressWarnings("unused")
